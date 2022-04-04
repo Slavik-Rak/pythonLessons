80d2a171f6e3db12bbe8e944e4f763ec70506ef7
Slavik-Rak <76218931+Slavik-Rak@users.noreply.github.com>
2022-04-04 20:28:26 +0300

    Foundations Python №2 - conditional expressions

---
+++ FoundationsPython/lesson2.py
@@ -0,0 +1,46 @@
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+# a = 5
+# b = 6
+
+# result = 5 == 6
+# print(result)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+#
+# bool1 = True
+# bool2 = False
+# print(bool1 == bool2)
+
+# and
+# or
+# not
+
+# age = int(input("Enter you age :"))
+# isMaried = bool(input("Enter your status :"))
+#
+# result = age > 18 and isMaried == True
+# print(result)
+
+# age = int(input("Enter you age :"))
+# isMaried = bool(input("Enter your status :"))
+#
+# result = age > 18 or isMaried
+# print(result)
+
+# age = int(input("Enter you age :"))
+#
+# print(not age > 21)
+#
+# in
+#
+# message = "Hello world"
+# a = input("Enter :")
+# print(a in message)
+
