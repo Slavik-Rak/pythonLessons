11e4fb136e4fe37488319796a3ff4eb6766c3938
Slavik-Rak <76218931+Slavik-Rak@users.noreply.github.com>
2022-04-04 19:54:32 +0300

    Foundations Python №1 - execution of tasks - study of standard functions

---
+++ FoundationsPython/lesson1.py
@@ -0,0 +1,24 @@
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# c = float(input("Enter 3 number: "))  # приймає від користувача значення типу float
+#
+# result = (a + b) // c
+#
+# print("You resoltat : ", result)
+
+# a = int(input("Enter type int : "))
+# b = str(input("Enter type str : "))
+# c = bool(input("Enter type bool : "))
+# d = float(input("Enter type float : "))
+#
+# print("Зміння a типу : ", type(a))
+# print("Зміння b типу : ", type(b))
+# print("Зміння c типу : ", type(c))
+# print("Зміння d типу : ", type(d))
+
+
+
+
+
+
+
