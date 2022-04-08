c58beb317ca1186624e81480b05a23f0428dc2b8
Slavik-Rak <76218931+Slavik-Rak@users.noreply.github.com>
2022-04-08 19:33:52 +0300

    lesson №4 - while -for -range

---
+++ FoundationsPython/lesson4.py
@@ -0,0 +1,49 @@
+# a = int(input("Enter number: "))
+#
+# while a < 10:
+#     print("Good: ", a)
+#     a += 1
+# else:
+#     print("Error!!")
+#
+# print("Finish")
+
+# while True:
+#     a = int(input("Enter number: "))
+#     if a == 1:
+#         print("Break")
+#         break
+#     else:
+#         print("continue")
+#         continue
+#
+# print("Finish")
+
+# name = "Slavik"
+#
+# for i in name:
+#     print(i)
+# else:
+#     print("Finish")
+
+# вкладені цикли (матриці)
+
+# i = int(input("Enter i: "))
+# j = int(input("Enter j: "))
+#
+# while i < 10:
+#     while j < 10:
+#         print(i * j, end="\t")
+#         j += 1
+#     print("\n")
+#     j = 1
+#     i += 1
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(2, 10):
+#     print(i)
+
+for i in range(1, 10, 3):
+    print(i)
--- HomeWork/homeWork1.py
+++ HomeWork/homeWork1.py
@@ -6,7 +6,7 @@ if a == 0 or a > 1000:
 else:
     if a % 10 == 1 and a % 100 != 11:
         print(a, " програміст")
-    elif 2 <= a % 10 <= 4 and 12 >= a % 100 >= 14:
+    elif 2 <= a % 10 <= 4 or 12 >= a % 100 >= 14:
         print(a, " програмістa")
     else:
         print(a, " програмістів")
